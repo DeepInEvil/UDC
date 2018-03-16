356759fc7635997442e0e6753db02faac5354e16
DeepInEvil <deba.kgec@gmail.com>
2018-03-16 12:19:15 +0100

    ``

--- run.py
+++ run.py
@@ -68,8 +68,8 @@ else:
     )
 
 # model = CNNDualEncoder(dataset.embed_dim, dataset.vocab_size, h_dim, dataset.vectors, args.gpu)
-model = LSTMDualEncoder(dataset.embed_dim, dataset.vocab_size, h_dim, dataset.vectors, args.gpu)
-#model = EmbMM(dataset.embed_dim, dataset.vocab_size, h_dim, dataset.vectors, args.gpu)
+#model = LSTMDualEncoder(dataset.embed_dim, dataset.vocab_size, h_dim, dataset.vectors, args.gpu)
+model = EmbMM(dataset.embed_dim, dataset.vocab_size, h_dim, dataset.vectors, args.gpu)
 # model = CCN_LSTM(dataset.embed_dim, dataset.vocab_size, h_dim, max_seq_len, k, dataset.vectors, args.gpu)
 
 solver = optim.Adam(model.parameters(), lr=args.lr)
