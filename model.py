50d842b2786bb1b412ab2e90a24a87c6e8aeea75
DeepInEvil <deba.kgec@gmail.com>
2018-03-16 22:15:48 +0100

    ``

--- model.py
+++ model.py
@@ -221,7 +221,7 @@ class EmbMM(nn.Module):
             response_h = r[i].view(self.h_dim, 1)
             ans = torch.mm(torch.mm(context_h, self.M), response_h)[0][0]
             #print (ans)
-            results.append(torch.sigmoid(ans))
+            results.append((ans))
             #response_encodings.append(response_h)
 
         results = torch.stack(results)
