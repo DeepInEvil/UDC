261eabaf59a964c02719f3aa42205c5a4a467538
DeepInEvil <deba.kgec@gmail.com>
2018-03-18 21:59:31 +0100

    ``

--- model.py
+++ model.py
@@ -324,7 +324,7 @@ class EmbMM(nn.Module):
             response_h = r[i].view(self.h_dim, 1)
             #response_h = r[i]
             w_mm = torch.mm(context_h, self.M)
-            #print (w_mm.size())
+            print (w_mm.size())
             ans = w_mm * response_h
             print (ans.size())
             results.append(self.maxpool(ans))
