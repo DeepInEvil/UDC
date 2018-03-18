6a2b4e12718810df52cef41ecabb12fb10f0689a
DeepInEvil <deba.kgec@gmail.com>
2018-03-18 17:32:49 +0100

    ``

--- run.py
+++ run.py
@@ -88,11 +88,11 @@ def main():
 
         for it, mb in train_iter:
             # Truncate input
-            print (mb.context)
-            context = mb.context[:, :args.max_context_len]
-            response = mb.response[:, :args.max_response_len]
+            print (mb.context.lenghts, mb.context)
+            #context = mb.context[:, :args.max_context_len]
+            #response = mb.response[:, :args.max_response_len]
             #print (context)
-            output = model(context, response)
+            output = model(mb.context, mb.response)
             loss = F.binary_cross_entropy_with_logits(output, mb.label)
 
             loss.backward()
