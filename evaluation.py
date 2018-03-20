cd9a2b96ca98a09db83a791380a1871403b441a5
DeepInEvil <deba.kgec@gmail.com>
2018-03-20 14:32:18 +0100

    clipped gradients

--- evaluation.py
+++ evaluation.py
@@ -74,7 +74,7 @@ def eval_pack_model(model, data_iter, max_context_len, max_response_len, gpu=Fal
         # Get scores for negative samples
         score_negs = [
             model(context, cntx_l, neg_dat[i], neg_lenghts[i] ).unsqueeze(1)
-            for i in range(1, 10)
+            for i in range(0, 9)
         ]
         # Total scores, positives at position zero
         scores_mb = torch.cat([score_pos, *score_negs], dim=1)
