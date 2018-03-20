e4d80e38df3ec25e4c3a37b31b2ff4ef45c829b2
DeepInEvil <deba.kgec@gmail.com>
2018-03-20 14:23:07 +0100

    clipped gradients

--- model.py
+++ model.py
@@ -192,6 +192,7 @@ class LSTMDualEncPack(nn.Module):
         --------
         o: vector of (batch_size)
         """
+        print (x2_l)
         x1_l, x1_p_idx = x1_l.sort(0, descending=True)
         x2_l, x2_p_idx = x2_l.sort(0, descending=True)
         c, r = self.forward_enc(x1[x1_p_idx], x1_l, x2[x2_p_idx], x2_l)
