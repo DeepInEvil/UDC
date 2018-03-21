8271e35911c7d3715c1fb485dec42d56d23373d2
DeepInEvil <deba.kgec@gmail.com>
2018-03-21 13:54:59 +0100

    packed rnn

--- model.py
+++ model.py
@@ -76,7 +76,7 @@ class LSTMDualEncoder(nn.Module):
     def __init__(self, emb_dim, n_vocab, h_dim=256, pretrained_emb=None, gpu=False):
         super(LSTMDualEncoder, self).__init__()
 
-        self.word_embed = nn.Embedding(n_vocab, emb_dim, padding_idx=0)
+        self.word_embed = nn.Embedding(n_vocab, emb_dim, padding_idx=1)
         #print (n_vocab)
         if pretrained_emb is not None:
              self.word_embed.weight.data.copy_(pretrained_emb)
