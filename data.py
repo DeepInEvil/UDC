cc720e83150673fa67437aeb617a2b5cc535d501
DeepInEvil <deba.kgec@gmail.com>
2018-03-21 14:05:47 +0100

    packed rnn

--- data.py
+++ data.py
@@ -180,7 +180,7 @@ class UDC:
 
         self.LABEL.build_vocab(self.train)
         print (vocab.stoi['__pad__'])
-        print (vocab.itos[0])
+        print (vocab.itos[1])
         self.dataset_size = len(self.train.examples)
         self.vocab_size = len(self.TEXT.vocab.itos)
         self.embed_dim = embed_dim
