217a871129d48fe4531ac0de0c7879f825be56f3
DeepInEvil <deba.kgec@gmail.com>
2018-03-22 19:22:52 +0100

    packed rnn

--- DeepAttention.py
+++ DeepAttention.py
@@ -136,7 +136,7 @@ class LSTMDualAttnEnc(nn.Module):
         attn_energies = attn.bmm(x).transpose(1, 2) #B,T,D * B,D,1 --> B,1,T
         alpha = F.softmax(attn_energies, dim=1)  # B,T
         alpha = alpha.unsqueeze(1)  # B,1,T
-        weighted_attn = alpha * x1
+        weighted_attn = alpha.bmm(x1)
 
         return weighted_attn
 
