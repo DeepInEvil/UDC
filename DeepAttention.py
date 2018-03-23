3f462409537f8a68de0ee6ed82c44846810f016f
DeepInEvil <deba.kgec@gmail.com>
2018-03-23 11:34:25 +0100

    packed rnn

--- DeepAttention.py
+++ DeepAttention.py
@@ -94,12 +94,13 @@ class LSTMDualAttnEnc(nn.Module):
         attn_energies = attn.bmm(x).transpose(1, 2) #B,T,D * B,D,1 --> B,1,T
         #print (attn_energies.size())
         attn_energies = attn_energies.squeeze(1).masked_fill(mask, -1e12)
-        alpha = F.softmax(attn_energies.mul_(self.scale), dim=-1)  # B,T
+        alpha = F.softmax(attn_energies, dim=-1)  # B,T
         alpha = alpha.unsqueeze(1)  # B,1,T
         #print (alpha.size(), x1.size())
         weighted_attn = alpha.bmm(x1)
 
         return weighted_attn.squeeze()
+
 
     def forward_fc(self, c, r):
         """
