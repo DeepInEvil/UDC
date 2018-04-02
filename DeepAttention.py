3cac6e4b3c2ca87f88e8eb4c286cdd61587d5f6a
DeepInEvil <deba.kgec@gmail.com>
2018-04-02 03:02:58 +0200

    packed rnn

--- DeepAttention.py
+++ DeepAttention.py
@@ -438,7 +438,7 @@ class GRUAttnmitKey(nn.Module):
             utrncs = context[i].cpu().data.numpy()
             for j, word in enumerate(utrncs):
                 if word in self.ubuntu_cmds.keys():
-                    key_emb[i][j] = self.ubuntu_cmds[word]
+                    key_emb[i][j] = torch.from_numpy(self.ubuntu_cmds[word])
 
         return Variable(key_emb.cuda())
 
