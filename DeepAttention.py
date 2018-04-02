54cac5edd033061feba7140341d6e5ea440bf726
DeepInEvil <deba.kgec@gmail.com>
2018-04-02 02:58:23 +0200

    packed rnn

--- DeepAttention.py
+++ DeepAttention.py
@@ -370,133 +370,6 @@ class GRUDualAttnEnc(nn.Module):
         return o
 
 
-class GRUAttenmitIntent(nn.Module):
-
-    def __init__(self, emb_dim, n_vocab, h_dim=256, pretrained_emb=None, pad_idx=0, gpu=False, emb_drop=0.7, max_seq_len=160):
-        super(GRUAttenmitIntent, self).__init__()
-
-        self.word_embed = nn.Embedding(n_vocab, emb_dim, padding_idx=pad_idx)
-
-        if pretrained_emb is not None:
-            self.word_embed.weight.data.copy_(pretrained_emb)
-
-        self.rnn = nn.GRU(
-            input_size=emb_dim, hidden_size=h_dim,
-            num_layers=1, batch_first=True
-        )
-
-        self.emb_drop = nn.Dropout(emb_drop)
-        self.M = nn.Parameter(torch.FloatTensor(h_dim + 1, h_dim))
-        self.b = nn.Parameter(torch.FloatTensor([0]))
-        self.attn = nn.Linear(h_dim, h_dim)
-        self.scale = 1. / math.sqrt(max_seq_len)
-        self.out_hidden = nn.Linear(h_dim, 1)
-        self.out_drop = nn.Dropout(0.5)
-        #self.attn_out = nn.Linear(h_dim, 1)
-        self.softmax = nn.Softmax()
-        self.init_params_()
-        self.queries = np.load('data/queries.npy')
-        #self.bn = nn.BatchN
-
-        if gpu:
-            self.cuda()
-
-    def init_params_(self):
-        nn.init.xavier_normal(self.M)
-
-        # Set forget gate bias to 2
-        size = self.rnn.bias_hh_l0.size(0)
-        self.rnn.bias_hh_l0.data[size//4:size//2] = 2
-
-        size = self.rnn.bias_ih_l0.size(0)
-        self.rnn.bias_ih_l0.data[size//4:size//2] = 2
-
-        #init.xavier_uniform(self.attn.data)
-
-    def getIntent(self, context):
-        inte_vec = torch.zeros(context.size(0))
-        for i in range(context.size(0)):
-            last_utr = context[i].split(63346)[-1].cpu().data.numpy()
-            #print (last_utr, self.queries)
-            for ids in last_utr:
-                if ids in self.queries:
-                    inte_vec[i] = 1
-
-        return Variable(inte_vec.unsqueeze(1)).cuda()
-
-    def forward(self, x1, x2, x1mask):
-        """
-        Inputs:
-        -------
-        x1, x2: seqs of words (batch_size, seq_len)
-
-        Outputs:
-        --------
-        o: vector of (batch_size)
-        """
-        intent = self.getIntent(x1)
-        sc, c, r = self.forward_enc(x1, x2)
-        c_attn = self.forward_attn(sc, r, x1mask)
-        c_out = torch.cat([c_attn, intent], dim=-1)
-        o = self.forward_fc(c_out, r)
-        #print (c_attn.size())
-
-        #o = F.tanh(self.out_hidden(c_attn))
-        #o = self.out_drop(o)
-        return o.view(-1)
-
-    def forward_enc(self, x1, x2):
-        """
-        x1, x2: seqs of words (batch_size, seq_len)
-        """
-        # Both are (batch_size, seq_len, emb_dim)
-        x1_emb = self.emb_drop(self.word_embed(x1))
-        x2_emb = self.emb_drop(self.word_embed(x2))
-
-        # Each is (1 x batch_size x h_dim)
-        sc, c = self.rnn(x1_emb)
-        _, r = self.rnn(x2_emb)
-
-        return sc, c.squeeze(), r.squeeze()
-
-    def forward_attn(self, x1, x, mask):
-        """
-        attention
-        :param x1: batch X seq_len X dim
-        :return:
-        """
-        max_len = x1.size(1)
-        b_size = x1.size(0)
-
-        x = x.squeeze(0).unsqueeze(2)
-        attn = self.attn(x1.contiguous().view(b_size*max_len, -1))# B*T,D -> B*T,D
-        #print (attn.size(), x.size())
-        attn = attn.view(b_size, max_len, -1) # B,T,D
-        attn_energies = attn.bmm(x).transpose(1, 2) #B,T,D * B,D,1 --> B,1,T
-        #print (attn_energies.size())
-        #attn_energies = attn_energies.squeeze(1).masked_fill(mask, -float('inf'))
-        #attn_energies = attn_energies * mask
-        #print (attn_energies.size(), mask.size())
-        attn_energies = attn_energies.squeeze(1) * mask  # B, T
-        alpha = F.softmax(attn_energies, dim=-1)  # B, T
-        alpha = alpha.unsqueeze(1)  # B,1,T
-        weighted_attn = alpha.bmm(x1)  # B,T
-
-        return weighted_attn.squeeze()
-
-    def forward_fc(self, c, r):
-        """
-        c, r: tensor of (batch_size, h_dim)
-        """
-        # (batch_size x 1 x h_dim)
-        o = torch.mm(c, self.M).unsqueeze(1)
-        # (batch_size x 1 x 1)
-        o = torch.bmm(o, r.unsqueeze(2))
-        o = o + self.b
-
-        return o
-
-
 class GRUAttnmitKey(nn.Module):
 
     def __init__(self, emb_dim, n_vocab, h_dim=256, pretrained_emb=None, pad_idx=0, gpu=False, emb_drop=0.7, max_seq_len=160):
@@ -523,6 +396,7 @@ class GRUAttnmitKey(nn.Module):
         self.softmax = nn.Softmax()
         self.init_params_()
         #self.bn = nn.BatchN
+        self.ubuntu_cmds = np.load('ubuntu_data/man_dict_vec.npy').item()
 
         if gpu:
             self.cuda()
@@ -558,12 +432,24 @@ class GRUAttnmitKey(nn.Module):
         #o = self.out_drop(o)
         return o.view(-1)
 
+    def forward_key(self, context):
+        key_emb = torch.zeros(context.size(0), context.size(1), 200)
+        for i in range(context.size(0)):
+            utrncs = context[i].cpu().data.numpy()
+            for j, word in enumerate(utrncs):
+                if word in self.ubuntu_cmds.keys():
+                    key_emb[i][j] = self.ubuntu_cmds[word]
+
+        return Variable(key_emb.cuda())
+
     def forward_enc(self, x1, x2):
         """
         x1, x2: seqs of words (batch_size, seq_len)
         """
         # Both are (batch_size, seq_len, emb_dim)
+        key_emb = self.forward_key(x1)
         x1_emb = self.emb_drop(self.word_embed(x1))
+        x1_emb = torch.cat([x1_emb, key_emb], dim=-1)
         x2_emb = self.emb_drop(self.word_embed(x2))
 
         # Each is (1 x batch_size x h_dim)
