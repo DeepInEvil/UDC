32e0e5c0fbb3bb6add8bdcc7f4e90b11ff845b50
DeepInEvil <deba.kgec@gmail.com>
2018-04-10 00:11:30 +0200

    packed rnn

--- DeepAttention.py
+++ DeepAttention.py
@@ -321,9 +321,8 @@ class GRUDualAttnEnc(nn.Module):
         attn = self.attn(x1.contiguous().view(b_size*max_len, -1))# B*T,D -> B*T,D
         attn = attn.view(b_size, max_len, -1) # B,T,D
         attn_energies = attn.bmm(x).transpose(1, 2) #B,T,D * B,D,1 --> B,1,T
-        attn_energies = attn_energies.squeeze(1) * mask  # B, T
-        alpha = F.softmax(attn_energies, dim=-1)  # B, T
-        print (alpha)
+        alpha = F.softmax(attn_energies.squeeze(1), dim=-1)  # B, T
+        alpha = alpha * mask  # B, T
         alpha = alpha.unsqueeze(1)  # B,1,T
         weighted_attn = alpha.bmm(x1)  # B,T
 
