35b9ccd9d51e32cfd6d44cfe6a688a4e8f4f0711
DeepInEvil <deba.kgec@gmail.com>
2018-04-18 02:25:11 +0200

    packed rnn

--- get_test_results.py
+++ get_test_results.py
@@ -111,8 +111,8 @@ def eval_model_v1(model, dataset, mode='valid', gpu=False, no_tqdm=False):
                 tot += 1
                 cntxt = get_words((context[j].cpu().data.numpy()))
                 correct_response = get_words((response[j].cpu().data.numpy()))
-                print (response[j+_].cpu().data.numpy())
-                predicted = get_words((response[j+_].cpu().data.numpy()))
+                #print (response[j+_].cpu().data.numpy())
+                predicted = get_words((response[j+_].cpu().data.numpy()[0]))
 
                 out_file.write(cntxt+'\t'+correct_response + '\t' + predicted)
                 out_file.write('\n')
