80509cf2fc20a0c29d258200a51b8e4ef029f6cb
DeepInEvil <deba.kgec@gmail.com>
2018-04-19 12:55:10 +0200

    packed rnn

--- data.py
+++ data.py
@@ -518,7 +518,7 @@ class UDCv3:
 
         for j, (row_c, row_r, row_y) in enumerate(zip(c, r, y)):
             #check if query
-            last_utr = row_c.split(63346)[-1]
+            last_utr = str(row_c).split(63346)[-1]
             if int(row_c[-1]) in self.q_idx or int(last_utr[0]) in self.w1h:
                 if int(row_r[-1]) in self.q_idx or int(row_r[-1]) in self.w1h:
                     q_l[j] = 1
