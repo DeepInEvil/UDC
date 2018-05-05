3b8c8b9982a502f488d5111922cbe1d37eed9080
DeepInEvil <deba.kgec@gmail.com>
2018-05-05 15:14:27 +0200

    packed rnn

--- data.py
+++ data.py
@@ -574,7 +574,7 @@ class UDCv4:
 
         self.desc_len = 80
 
-        with open(f'{path}/dataset_1m_s.pkl', 'rb') as f:
+        with open(f'{path}/dataset_1M', 'rb') as f:
             dataset = pickle.load(f, encoding='ISO-8859-1')
             self.train, self.valid, self.test = dataset
 
