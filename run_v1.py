0333322373b9b3c513f4e7fcff7b9aaa4c13aa32
DeepInEvil <deba.kgec@gmail.com>
2018-05-21 14:28:19 +0200

    packed rnn

--- run_v1.py
+++ run_v1.py
@@ -117,10 +117,11 @@ def main():
         print('Loss: {:.3f}; recall@1: {:.3f}; recall@2: {:.3f}; recall@5: {:.3f}'
               .format(loss.data[0], recall_at_ks[0], recall_at_ks[1], recall_at_ks[4]))
 
-        #if epoch > 5:
-        #     eval_test()
+        if epoch > 10:
+            eval_test()
 
         save_model(model, 'GRU_kb_enc_gru5')
+
 
 
 def eval_test():
@@ -137,7 +138,7 @@ def eval_test():
 
 try:
     main()
-    eval_test()
+    #eval_test()
 except KeyboardInterrupt:
     eval_test()
     exit(0)
