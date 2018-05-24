71ab891f7b7f354476d58d0cdf78a2d536086dfb
DeepInEvil <deba.kgec@gmail.com>
2018-05-24 14:18:52 +0200

    packed rnn

--- get_attention_weights.py
+++ get_attention_weights.py
@@ -9,7 +9,7 @@ import torch.nn.init as init
 from model import CNNDualEncoder, LSTMDualEncoder, CCN_LSTM, EmbMM
 from data import UDCv1, UDCv2, UDCv4
 from util import save_model, clip_gradient_threshold
-from DeepAttention import LSTMDualAttnEnc, LSTMPAttn, GRUDualAttnEnc, GRUAttnmitKey, LSTMKeyAttn, GRUAttn_KeyCNN3
+from DeepAttention import LSTMDualAttnEnc, LSTMPAttn, GRUDualAttnEnc, GRUAttnmitKey, LSTMKeyAttn, GRUAttn_KeyCNN2
 from util import load_model
 
 #load vocab and other functions
@@ -50,7 +50,7 @@ def get_atten_dict(sent, weights):
 udc = UDCv4('ubuntu_data', batch_size=10, use_mask=True,
             max_seq_len=320, gpu=True, use_fasttext=True)
 
-model = GRUAttn_KeyCNN3(udc.emb_dim, udc.vocab_size, 300, udc.vectors, 0, True)
+model = GRUAttn_KeyCNN2(udc.emb_dim, udc.vocab_size, 300, udc.vectors, 0, True)
 model = load_model(model, 'GRU_kb_enc_gru5')
 model.eval()
 
