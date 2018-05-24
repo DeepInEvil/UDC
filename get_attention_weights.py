f129c0b5a3cb4331bf0ec2a4bc3902e8f6a3d27f
DeepInEvil <deba.kgec@gmail.com>
2018-05-24 15:34:06 +0200

    packed rnn

--- get_attention_weights.py
+++ get_attention_weights.py
@@ -5,7 +5,7 @@ from torch.autograd import Variable
 import torch.nn.functional as F
 import math
 import torch.nn.init as init
-
+from tqdm import tqdm
 from model import CNNDualEncoder, LSTMDualEncoder, CCN_LSTM, EmbMM
 from data import UDCv1, UDCv2, UDCv4
 from util import save_model, clip_gradient_threshold
@@ -47,7 +47,7 @@ def get_atten_dict(sent, weights):
     return out_attn
 
 
-udc = UDCv4('ubuntu_data', batch_size=10, use_mask=True,
+udc = UDCv4('ubuntu_data', batch_size=64, use_mask=True,
             max_seq_len=320, gpu=True, use_fasttext=True)
 
 model = GRUAttn_KeyCNN2(udc.emb_dim, udc.vocab_size, 300, udc.vectors, 0, True)
@@ -56,11 +56,30 @@ model.eval()
 
 data_iter = udc.get_iter('test')
 
-scores = []
+attentions = []
+data_iter = tqdm(data_iter)
+data_iter.set_description_str('Evaluation')
+n_data = udc.n_test
+data_iter.total = n_data // udc.batch_size
+
 for mb in data_iter:
-    context, response, y, cm, rm, _, key_r, key_mask_r = mb
-    key_mask_r = key_mask_r.unsqueeze(2).repeat(1, 1, 50 * 4)
+    context, response, y, cm, rm, _, key_r, key_m_r = mb
+    key_mask_r = key_m_r.unsqueeze(2).repeat(1, 1, 50 * 4)
+    scores_mb = F.sigmoid(model(context, response, cm, rm, key_r, key_mask_r)).cpu().data.numpy()
     key_emb_r = model.get_weighted_key(key_r, key_mask_r)
     sc, sr, c, r = model.forward_enc(context, response, key_emb_r)
-    attentions = model.forward_attn(sr, c, rm).alpha.squeeze().cpu().data.numpy
-    print (attentions)
+
+    max_len = sr.size(1)
+    b_size = sr.size(0)
+
+    c = c.squeeze(0).unsqueeze(2)
+    attn = model.attn(sr.contiguous().view(b_size * max_len, -1))  # B*T,D -> B*T,D
+    attn = attn.view(b_size, max_len, -1)  # B,T,D
+    attn_energies = (attn.bmm(c).transpose(1, 2))  # B,T,D * B,D,1 --> B,1,T
+    alpha = F.softmax(attn_energies.squeeze(1), dim=-1)  # B, T
+    alpha = alpha * rm
+    for i, rb in enumerate(response):
+        if (torch.sum(key_m_r[i]).cpu().data.numpy()) > 0 and scores_mb[i] > 0.5:
+            attentions.append(get_atten_dict(response[i].cpu().data.numpy(), alpha[i].cpu().data.numpy()))
+
+np.save('attention_key.npy', attentions)
