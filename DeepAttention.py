99c7dacd255039851e8e27fcc641af01aaa3b46f
DeepInEvil <deba.kgec@gmail.com>
2018-05-25 16:42:25 +0200

    packed rnn

--- DeepAttention.py
+++ DeepAttention.py
@@ -804,7 +804,7 @@ class GRUAttn_KeyCNN4(nn.Module):
         o: vector of (batch_size)
         """
         key_mask_r = key_mask_r.unsqueeze(2).repeat(1, 1, self.n_filter * 4)
-        #print (torch.sum(key_mask_r))
+        #git pprint (torch.sum(key_mask_r))
         key_emb_r = self.get_weighted_key(key_r, key_mask_r)
         sc, sr, c, r = self.forward_enc(x1, x2, key_emb_r)
         c_attn = self.forward_attn(sc, r, x1mask)
@@ -850,7 +850,7 @@ class GRUAttn_KeyCNN4(nn.Module):
         c_k = c.unsqueeze(1).repeat(1, key_emb_r.size(1), 1)
 
         x2_emb = self.emb_drop(self.word_embed(x2))
-        z = F.sigmoid(torch.mm(c_k.view(b*s, -1), self.Wc).view(b, s, self.emb_dim) + torch.mm(key_emb_r.view(b*s, -1), self.We).view(b, s, self.emb_dim))
+        z = F.softmax(torch.mm(c_k.view(b*s, -1), self.Wc).view(b, s, self.emb_dim) + torch.mm(key_emb_r.view(b*s, -1), self.We).view(b, s, self.emb_dim))
 
         x2_emb = (1 - z) * x2_emb + z * key_emb_r
         # Each is (1 x batch_size x h_dim)
