12d2b70a836b68dd52a9625db8c55ccb6c60797b
DeepInEvil <deba.kgec@gmail.com>
2018-05-30 15:36:35 +0200

    packed rnn

--- get_test_results.py
+++ get_test_results.py
@@ -9,15 +9,15 @@ import torch.nn.init as init
 from model import CNNDualEncoder, LSTMDualEncoder, CCN_LSTM, EmbMM
 from data import UDCv1, UDCv2, UDCv4
 from util import save_model, clip_gradient_threshold
-from DeepAttention import LSTMDualAttnEnc, LSTMPAttn, GRUDualAttnEnc, GRUAttnmitKey, LSTMKeyAttn, GRUAttn_KeyCNN3
+from DeepAttention import LSTMDualAttnEnc, LSTMPAttn, GRUDualAttnEnc, GRUAttnmitKey, LSTMKeyAttn, GRUAttn_KeyCNN4
 from util import load_model
 
 from tqdm import tqdm
 
-udc = UDCv4('ubuntu_data', batch_size=10, use_mask=True,
+udc = UDCv4('ubuntu_data', batch_size=64, use_mask=True,
             max_seq_len=320, gpu=True, use_fasttext=True)
 
-model = GRUAttn_KeyCNN3(
+model = GRUAttn_KeyCNN4(
     udc.emb_dim, udc.vocab_size, 300, udc.vectors, 0, True
 )
 
@@ -48,7 +48,7 @@ def eval_test():
     print('-------------------------------')
 
     recall_at_ks = eval_model_v1(
-        model, udc, 'test', gpu=True, no_tqdm=True
+        model, udc, 'test', gpu=True, no_tqdm=False
     )
 
     print('Recall@1: {:.3f}; recall@2: {:.3f}; recall@5: {:.3f}'
@@ -92,7 +92,7 @@ def eval_model_v1(model, dataset, mode='valid', gpu=False, no_tqdm=False):
         n_data = dataset.n_valid if mode == 'valid' else dataset.n_test
         data_iter.total = n_data // dataset.batch_size
 
-    out_file = open('ubuntu_data/wrong_preds.txt', 'w')
+    out_file = open('ubuntu_data/wrong_pred_dke.txt', 'w')
     tot = 0.0
     correct = 0.0
     for mb in data_iter:
@@ -101,10 +101,10 @@ def eval_model_v1(model, dataset, mode='valid', gpu=False, no_tqdm=False):
         # Get scores
         scores_mb = F.sigmoid(model(context, response, cm, rm, key_r, key_mask_r))
         scores_mb = scores_mb.cpu() if gpu else scores_mb
-
-        for j in range(0, len(context), 10):
-            corr, _ = evaluate_recall(scores_mb[j:j+10])
-            if corr:
+        pred = np.argmax(scores_mb)
+        for j, con in enumerate(context):
+            #corr, _ = evaluate_recall(scores_mb[j:j+10])
+            if pred == 0 and y[j].cpu().data.numpy() == 1:
                 correct += 1
                 tot += 1
             else:
@@ -121,8 +121,6 @@ def eval_model_v1(model, dataset, mode='valid', gpu=False, no_tqdm=False):
     print (tot)
     print (correct/tot)
     scores = np.concatenate(scores)
-    print (scores.shape)
-    #scores = scores[:-(scores.shape[0] % 10)]
     mod = scores.shape[0] % 10
     scores = scores[:-mod if mod != 0 else None]
 
@@ -132,6 +130,6 @@ def eval_model_v1(model, dataset, mode='valid', gpu=False, no_tqdm=False):
     return recall_at_ks
 
 
-model = load_model(model, 'GRU_kb_enc_gru3_bak750')
+model = load_model(model, 'DKE-GRU')
 
 eval_test()
