9626f76f06cffd9d6dd4ce10980cb5f6d80f666f
DeepInEvil <deba.kgec@gmail.com>
2018-05-30 02:20:44 +0200

    packed rnn

--- get_attention_weights.py
+++ get_attention_weights.py
@@ -65,12 +65,13 @@ udc = UDCv4('ubuntu_data', batch_size=10, use_mask=True,
             max_seq_len=320, gpu=True, use_fasttext=True)
 
 model = GRUAttn_KeyCNN4(udc.emb_dim, udc.vocab_size, 300, udc.vectors, 0, True)
-model = load_model(model, 'GRU_kb_enc_best')
+model = load_model(model, 'DKE-GRU.')
 model.eval()
 
 data_iter = udc.get_iter('test')
 
 attentions = []
+contexts = []
 data_iter = tqdm(data_iter)
 data_iter.set_description_str('Testing')
 n_data = udc.n_test
@@ -101,11 +102,21 @@ for mb in data_iter:
     attn_energies = (attn.bmm(c).transpose(1, 2))  # B,T,D * B,D,1 --> B,1,T
     alpha = F.softmax(attn_energies.squeeze(1), dim=-1)  # B, T
     alpha = alpha * rm
+
+    max_len = sc.size(1)
+    b_size = sc.size(0)
+    attn_c = model.attn(sc.contiguous().view(b_size * max_len, -1))  # B*T,D -> B*T,D
+    attn_c = attn_c.view(b_size, max_len, -1)  # B,T,D
+    attn_energies_c = (attn_c.bmm(r).transpose(1, 2))  # B,T,D * B,D,1 --> B,1,T
+    alphac = F.softmax(attn_energies_c.squeeze(1), dim=-1)  # B, T
+    alphac = alphac * rm
+
     pred = np.argmax(scores_mb)
     for i, rb in enumerate(response):
-        if (torch.sum(key_m_r[i]).cpu().data.numpy()) > 0:
+        if (torch.sum(key_m_r[i]).cpu().data.numpy()) > 0 and y[i].cpu().data.numpy() == 1 and pred == 0:
             if pred == 0:
                 attentions.append(get_atten_dict(response[i].cpu().data.numpy(), alpha[i].cpu().data.numpy()))
+                contexts.append(get_atten_dict(context[i].cpu().data.numpy(), alphac[i].cpu().data.numpy()))
                 correct += 1
                 total += 1
             else:
